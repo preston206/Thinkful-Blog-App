7ca87021ebd1fef1e751093dd4ac0a2d30922dde
preston <broberts9@gmail.com>
2017-08-06 23:41:22 -0700

    updating server.js

--- server.js
+++ server.js
@@ -107,3 +107,48 @@ app.delete('/posts/:id', (req, res) => {
         .then(blog => res.status(204).end())
         .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
+
+let server;
+
+// connect to db, then starts the server
+function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+
+    return new Promise((resolve, reject) => {
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve();
+            })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
+    });
+}
+
+// close server
+function closeServer() {
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+    runServer().catch(err => console.error(err));
+};
+
+module.exports = { app, runServer, closeServer };
