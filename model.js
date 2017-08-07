461b58db4093f6a21e454658aa08e33224668948
preston <broberts9@gmail.com>
2017-08-07 16:19:46 -0700

    setting up routes and api structure

--- model.js
+++ model.js
@@ -7,25 +7,25 @@ const blogSchema = mongoose.Schema({
         firstName: { type: String, required: true },
         lastName: { type: String, required: true }
     },
-    content: { type: String, required: true },
-    created: { type: Date }
+    content: { type: String },
+    created: { type: Date, default: Date.now }
 });
 
-// virtual for full name
-blogSchema.virtual('authorFullName').get(() => {
+// virtual for full name; NOTE: don't use ES6 arrow here- it will error
+blogSchema.virtual('authorFullName').get(function() {
     return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
-blogSchema.methods.apiRep = function() {
+blogSchema.methods.apiRep = function () {
     return {
         id: this._id,
-        created: this.created,
         title: this.title,
+        content: this.content,
         author: this.authorFullName,
-        content: this.content
+        created: this.created
     };
 };
 
 const Blog = mongoose.model('Blog', blogSchema);
 
-module.exports = {Blog};
+module.exports = { Blog };
--- server.js
+++ server.js
@@ -1,32 +1,36 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
-
-mongoose.Promise = global.Promise;
-
-app.use(bodyParser.json());
+const morgan = require('morgan');
 
 const { PORT, DATABASE_URL } = require('./config');
 const { Blog } = require('./model');
 
+const app = express();
 
-// get request for 5 blog posts
+mongoose.Promise = global.Promise;
+
+app.use(morgan('common'));
+app.use(bodyParser.json());
+
+
+
+
+// get blog posts
 app.get('/posts', (req, res) => {
+    console.log("getting data; brb");
     Blog
         .find()
-        .limit(5)
+        .limit(3)
         .exec()
-        .then(blogs => {
+        .then(posts => { //NOTE: "posts" is not arbitrary- it is the actual name of the collection! I spent hours troubleshooting that!
             res.json({
-                blogs: blogs.map(
-                    (blog) => blog.apiRep())
+                "Blog Posts": posts.map(post => post.apiRep())
             });
         })
-        .catch(
-        error => {
+        .catch(error => {
             console.error(error);
-            res.status(500).json({ message: "Internal server error" });
+            res.status(500).json({ "error message": "we encountered an error when attempting to get your data" });
         });
 });
 
@@ -36,17 +40,17 @@ app.get('/posts/:id', (req, res) => {
     Blog
         .findById(req.params.id)
         .exec()
-        .then(blog => res.json(blog.apiRep()))
-        .catch(err => {
-            console.error(err);
-            res.status(500).json({ message: 'Internal server error' })
+        .then(post => res.json(post.apiRep()))
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({ "error message": 'we encountered an error when attempting to get your data' })
         });
 });
 
 
 // create blog post
 app.post('/posts/', (req, res) => {
-    const requiredFields = ['title', 'firstName', 'lastName', 'content'];
+    const requiredFields = ['title', 'content', 'author'];
     for (let i = 0; i < requiredFields.length; i++) {
         const field = requiredFields[i];
         if (!(field in req.body)) {
@@ -59,12 +63,11 @@ app.post('/posts/', (req, res) => {
     Blog
         .create({
             title: req.body.title,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
+            author: req.body.author,
             content: req.body.content
         })
         .then(
-        blog => res.status(201).json(blog.apiRep()))
+        post => res.status(201).json(post.apiRep()))
         .catch(err => {
             console.error(err);
             res.status(500).json({ message: 'Internal server error' });
@@ -83,7 +86,7 @@ app.put('/posts/:id', (req, res) => {
     }
 
     const toUpdate = {};
-    const updateableFields = ['title', 'firstName', 'lastName', 'content'];
+    const updateableFields = ['title', 'author', 'content'];
 
     updateableFields.forEach(field => {
         if (field in req.body) {
@@ -94,7 +97,7 @@ app.put('/posts/:id', (req, res) => {
     Blog
         .findByIdAndUpdate(req.params.id, { $set: toUpdate })
         .exec()
-        .then(blog => res.status(204).end())
+        .then(post => res.status(204).end())
         .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
@@ -104,7 +107,7 @@ app.delete('/posts/:id', (req, res) => {
     Blog
         .findByIdAndRemove(req.params.id)
         .exec()
-        .then(blog => res.status(204).end())
+        .then(post => res.status(204).end())
         .catch(err => res.status(500).json({ message: 'Internal server error' }));
 });
 
@@ -116,6 +119,7 @@ function runServer(databaseUrl = DATABASE_URL, port = PORT) {
     return new Promise((resolve, reject) => {
         mongoose.connect(databaseUrl, err => {
             if (err) {
+                console.log(err);
                 return reject(err);
             }
             server = app.listen(port, () => {
@@ -123,6 +127,7 @@ function runServer(databaseUrl = DATABASE_URL, port = PORT) {
                 resolve();
             })
                 .on('error', err => {
+                    console.log(err);
                     mongoose.disconnect();
                     reject(err);
                 });
